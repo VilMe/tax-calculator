a97b5fa6b49ff5ac9aae491931f3798cc85b6b67
VilMe <vilma.medrano00@gmail.com>
2025-10-08 22:06:08 -0500

    begini to define TaxCalculator class

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import customtkinter as ctk
 
+class TaxCalculator:
+    def __init__(self):
+        # Initialize our window
+        self.window = ctk.CTk()
+        self.window.title('Tax Calculator')
+        self.window.geometry('280x200')
+        self.window.resizable(False, False)
 
+
+         # Initialize our window
+        self.padding: dict = {'padx': 20, 'pady': 10}
+        
+        # Income label and entry
