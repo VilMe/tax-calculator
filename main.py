ca77db0a5d283768871ff8be50607df043441cbd
VilMe <vilma.medrano00@gmail.com>
2025-10-10 18:38:30 -0500

    define function to calculate tax, update tax entry variable to tax rate for better description

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ class TaxCalculator:
         self.income_entry.grid(row=0, column=1, **self.padding)
 
         # Income label and entry
-        self.tax_label = ctk.CTkLabel(self.window, text='Percent: ')
-        self.tax_label.grid(row=1, column=0, **self.padding)
-        self.tax_entry = ctk.CTkEntry(self.window)
-        self.tax_entry.grid(row=1, column=1, **self.padding)
+        self.tax_rate_label = ctk.CTkLabel(self.window, text='Percent: ')
+        self.tax_rate_label.grid(row=1, column=0, **self.padding)
+        self.tax_rate_entry = ctk.CTkEntry(self.window)
+        self.tax_rate_entry.grid(row=1, column=1, **self.padding)
 
         # Result label annd entry
         self.result_label = ctk.CTkLabel(self.window, text='Tax: ')
@@ -32,10 +32,20 @@ class TaxCalculator:
         self.result_entry.grid(row=2, column=1, **self.padding)
 
         # Calculate button
-        self.calculate_button = ctk.CTkButtonn(self.window, text='Calculate', command=self.calculate_tax)
+        self.calculate_button = ctk.CTkButton(self.window, text='Calculate', command=self.calculate_tax)
         self.calculate_button.grid(row=3, column=1, **self.padding)
-        
 
+    def update_result(self, text: str):
+        self.result_entry.delete(0, ctk.END)
+        self.result_entry.insert(0, text)
+
+    def calculate_tax(self):
+        try:
+            income: float = float(self.income_entry.get())
+            tax_rate: float = float(self.tax_rate_entry.get())
+            self.update_result(f'${income * (tax_rate / 100):,.2f}')
+        except ValueError:
+            self.update_result('Invalid input')
 
     def run(self):
         self.window.mainloop()
