785f741725acb0208e8b31ac35e16042016fc129
VilMe <vilma.medrano00@gmail.com>
2025-10-09 22:05:49 -0500

    add second text and box for input of perecentage to be deducted

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ class TaxCalculator:
          # Initialize our window
         self.padding: dict = {'padx': 20, 'pady': 10}
         
-        # Income label and entry
-
+        # Income label and entr
         self.income_label = ctk.CTkLabel(self.window, text='Income: ')
         self.income_label.grid(row=0, column=0, **self.padding)
         self.income_entry = ctk.CTkEntry(self.window)
         self.income_entry.grid(row=0, column=1, **self.padding)
+
+        # Income label and entr
+        self.tax_label = ctk.CTkLabel(self.window, text='Percent: ')
+        self.tax_label.grid(row=1, column=0, **self.padding)
+        self.tax_entry = ctk.CTkEntry(self.window)
+        self.tax_entry.grid(row=1, column=1, **self.padding)
 
     def run(self):
         self.window.mainloop()
