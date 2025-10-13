3650354f224444de263c06591227f7f604752ed2
VilMe <vilma.medrano00@gmail.com>
2025-10-12 21:14:11 -0500

    add button for 33% taxes which is common in USA for the peasants

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class TaxCalculator:
         # Initialize our window
         self.window = ctk.CTk()
         self.window.title('Tax Calculator')
-        self.window.geometry('280x200')
+        self.window.geometry('280x250')
         self.window.resizable(False, False)
 
 
@@ -32,9 +32,13 @@ class TaxCalculator:
         self.result_entry.insert(0, '0')
         self.result_entry.grid(row=2, column=1, **self.padding)
 
+        # 33% calculate button
+        self.standard_33_button = ctk.CTkButton(self.window, text="33% Tax", command=self.button_event)
+        self.standard_33_button.grid(row=3, column=1, **self.padding)
+
         # Calculate button
         self.calculate_button = ctk.CTkButton(self.window, text='Calculate', command=self.calculate_tax)
-        self.calculate_button.grid(row=3, column=1, **self.padding)
+        self.calculate_button.grid(row=4, column=1, **self.padding)
 
     def update_result(self, text: str):
         self.result_entry.delete(0, ctk.END)
@@ -48,6 +52,15 @@ class TaxCalculator:
             self.update_result(f'${income * (tax_rate / 100):,.2f}')
         except ValueError:
             self.update_result('Invalid input')
+
+    def button_event(self):
+        try:
+            income: float = float(self.income_entry.get())
+            self.update_result(f'${income * (0.33):,.2f}')
+        except ValueError:
+            self.update_result('Invalid input')
+
+    
 
     def run(self):
         self.window.mainloop()
